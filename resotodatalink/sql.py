7c7d4aaaca70aa5d1489e83317eda93a78d838ae
Matthias Veit <aquamatthias@users.noreply.github.com>
2023-06-27 14:50:57 +0200

    feat: allow running sql statements (#8)

--- resotodatalink/sql.py
+++ resotodatalink/sql.py
@@ -100,7 +100,7 @@ def sql_kind_to_column_type(kind_name: str, model: Model, **kwargs: Any) -> Any:
         return String(255)
     elif kind_name == "boolean":
         return Boolean
-    elif kind and kind.properties:  # complex kind
+    elif kind and kind.properties is not None:  # complex kind
         return JSON
     elif kind and kind.runtime_kind is not None:  # refined simple type like enum
         return sql_kind_to_column_type(kind.runtime_kind, model)
